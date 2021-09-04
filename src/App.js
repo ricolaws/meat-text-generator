ac0484774e1c06e26a9524736a8824946627fc40
ricolaws <eatfrenzy@gmail.com>
2021-09-03 17:32:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      text: "",
+    };
+  }
+
+  componentWillMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios
+      .get(
+        "https://baconipsum.com/api/?type=meat-and-filler&paras=" +
+          this.state.paras
+      )
+      .then((response) => {
+        this.setState({ text: response.data }, function () {
+          for (const p of this.state.text) {
+            console.log(p);
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // getSampleText() {
+  //   axios
+  //     .get(
+  //       "http://hipsterjesus.com/api?paras=" +
+  //         this.state.paras +
+  //         "&html=" +
+  //         this.state.html
+  //     )
+  //     .then((response) => {
+  //       this.setState({ text: response.data.text }, function () {
+  //         console.log(this.state);
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+
+  render() {
+    return <div className="App">hello hello</div>;
+  }
 }
 
 export default App;
